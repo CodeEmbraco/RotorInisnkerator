7a5196846f638d79d5cdae064fb9bbc623179d63
h-rub-ksms <hever.rubio@ksms.mx>
2024-04-22 23:03:34 -0600

    logs view

---
+++ src/partials/logs/LogsTable.jsx
@@ -0,0 +1,232 @@
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import {
+  getOpenOrdersList,
+  selectOpenOrdersList,
+} from "../../store/slice/ordersSlice";
+
+import { getLogs, selectComponents, selectLogs, selectPallet } from "../../store/slice/palletsSlice";
+import LogsItem from "./LogsTableItem";
+import { ArrowCircleRight, ArrowLeft, ArrowRight, ArrowRight2 } from "iconsax-react";
+function LogsTable() {
+
+    const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [pallets, setPallets] = useState([]);
+
+  useEffect(() => {
+    fetchPallets();
+  }, [currentPage]);
+
+  const fetchPallets = async () => {
+    try {
+      const response = await fetch(`http://10.13.225.20:8004/api/v1/paletization/logs/?page=${currentPage}&page_size=10&workstation=MX4FA00P`);
+      const data = await response.json();
+      setPallets(data.results.pallets);
+      setTotalPages(Math.ceil(data.count / 10)); // Assuming 10 pallets per page
+    } catch (error) {
+      console.error('Error fetching pallets:', error);
+    }
+  };
+
+    const handlePrevClick = () => {
+        if (currentPage > 1) {
+          setCurrentPage(currentPage - 1);
+        }
+      };
+    
+      const handleNextClick = () => {
+        if (currentPage < totalPages) {
+          setCurrentPage(currentPage + 1);
+        }
+      };
+    
+  const orders = [
+    {
+      id: "0",
+      name: "102343261",
+      email: "515380138",
+      location: "UM2U3115U",
+      orders: "363",
+      lastOrder: "351",
+      spent: "2023-06-14",
+      refunds: "-",
+      fav: true,
+    },
+    {
+      id: "1",
+      name: "102349877",
+      email: "513301867..ZP",
+      location: "EM3Y60HLP",
+      orders: "500",
+      lastOrder: "0",
+      spent: "2023-06-07",
+      refunds: "4",
+      fav: false,
+    },
+    {
+      id: "2",
+      name: "102355400",
+      email: "113341219",
+      location: "---",
+      orders: "44200",
+      lastOrder: "7688",
+      spent: "2023-06-20",
+      refunds: "1",
+      fav: true,
+    },
+  ];
+
+  const [selectAll, setSelectAll] = useState(false);
+  const [isCheck, setIsCheck] = useState([]);
+
+  const dispatch = useDispatch();
+  const openOrdersList = useSelector(selectOpenOrdersList);
+  const [list, setList] = useState(useSelector(selectOpenOrdersList));
+
+  const palletSelected = useSelector(selectPallet);
+
+  const componentsList = useSelector(selectComponents);
+
+  const logs = useSelector(selectLogs)
+
+  useEffect(() => {
+    setList(componentsList);
+  }, []);
+
+  useEffect(() => {
+    setList(openOrdersList);
+  }, [openOrdersList]);
+
+  useEffect(() => {
+
+    dispatch(getLogs())
+
+    setPallets(logs);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+
+  return (
+    <div className="bg-whiterounded-sm relative w-full">
+    <header className="px-5 py-4 flex justify-between sticky top-0 bg-white z-10">
+      <h2 className="text-left font-semibold text-slate-800">
+        Lotes registrados:{" "}
+        <span className="text-slate-400 font-medium">
+          {Object.keys(logs).length > 0 ? logs.count : "0"}
+        </span>
+      </h2>
+      <nav className="flex justify-between" role="navigation" aria-label="Navigation">
+        <div className="mr-2">
+          <button onClick={handlePrevClick} disabled={currentPage === 1}>
+            {currentPage === 1 ? (
+              <a className="btn bg-white border-slate-200 text-slate-300 cursor-not-allowed">
+                <ArrowLeft size={20} color="#CBD5E1" />
+                <span className="hidden sm:inline">&nbsp;Previo</span>
+              </a>
+            ) : (
+              <a className="btn bg-white border-slate-200 hover:border-slate-300 text-black">
+                <ArrowLeft size={20} /> <span className="hidden sm:inline">&nbsp;Previo</span>
+              </a>
+            )}
+          </button>
+        </div>
+
+        <div className="text-right ml-2">
+          <button onClick={handleNextClick} disabled={currentPage === totalPages}>
+            {currentPage === totalPages ? (
+              <a className="btn bg-white border-slate-200 text-slate-300 cursor-not-allowed">
+                <span className="hidden sm:inline">Siguiente&nbsp;</span>
+                <ArrowRight size={20} color="#CBD5E1" />
+              </a>
+            ) : (
+              <a className="btn bg-white border-slate-200 hover:border-slate-300 text-black">
+                <span className="hidden sm:inline">Siguiente&nbsp;</span>
+                <ArrowRight size={20} />
+              </a>
+            )}
+          </button>
+        </div>
+      </nav>
+    </header>
+      <div>
+        {/* Table */}
+        <div className="overflow-x-auto">
+          <table className="table-auto w-full">
+            {/* Table header */}
+            <thead className="text-xs font-semibold uppercase text-slate-500 bg-slate-50 border-t border-b border-slate-200">
+              <tr>
+                <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                  <div className="font-semibold text-left">ID</div>
+                </th>
+                <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                  <div className="font-semibold">Lote</div>
+                </th>
+                <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                  <div className="font-semibold text-center">Producto</div>
+                </th>
+                <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                  <div className="font-semibold text-center">Orden</div>
+                </th>
+                {/* <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                  <div className="font-semibold text-left">
+                    Código de mat. (Compresor)
+                  </div>
+                </th>
+                <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                  <div className="font-semibold text-left">
+                    Serial (Compresor)
+                  </div>
+                </th> */}
+                <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                  <div className="font-semibold text-center">Cantidad</div>
+                </th>
+                <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                  <div className="font-semibold text-center">Fecha de creación</div>
+                </th>
+                <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                  <div className="font-semibold text-left">SAP Status</div>
+                </th>
+                <th className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+                  <div className="font-semibold text-center">Acciones</div>
+                </th>
+              </tr>
+              
+            </thead>
+            {/* Table body */}
+            <tbody className="text-sm divide-y divide-slate-200">
+              {Object.keys(pallets).length > 0 && pallets.length > 0
+                ? pallets.map((component, index) => {
+                  const rowNumber = index + 1;
+                    return (
+                      <LogsItem
+                        key={component.id}
+                        rowNumber={rowNumber}
+                        id={component.id}
+                        product={component.product}
+                        order={component.order}
+                        identifier={component.identifier}
+                        quantity={component.quantity}
+                        datetimeCreated={component.datetime_created}
+                        sendToSAP={component.send_to_sap}
+                        sapStatus={component.sap_status}
+                        sapSuccess={component.sap_success}
+                
+                        
+                      />
+                    );
+                  })
+                : ""}
+            </tbody>
+          </table>
+          <div>
+      
+    </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LogsTable;
---
+++ src/partials/logs/LogsTableItem.jsx
@@ -0,0 +1,190 @@
+import React, { useState } from "react";
+
+import { useDispatch, useSelector } from "react-redux";
+
+import {
+  setOrderSelected,
+  getMetadataFromOrder,
+} from "../../store/slice/orderSelectedSlice";
+
+import { addEvent } from "../../store/slice/eventsLogSlice";
+import {
+  unmountComponent,
+  unmountComponentAPI,
+  selectPallet,
+  reprocessPallet,
+} from "../../store/slice/palletsSlice";
+import ModalBlank from "../../components/ModalBlank";
+import { Check, Clock, InfoCircle, Verify, Warning2 } from "iconsax-react";
+
+function LogsItem(props) {
+  const dispatch = useDispatch();
+  const [dangerModalOpen, setDangerModalOpen] = useState(false);
+  const palletSelected = useSelector(selectPallet);
+
+  const handleReprocess = () => {
+    // Llama a la acción para eliminar el componente por su id
+    dispatch(reprocessPallet(props.identifier));
+    //dispatch(unmountComponent(props));
+  };
+
+  // Función para formatear la fecha y hora
+  function formatDateTime(datetime) {
+    const dateObj = new Date(datetime);
+
+    const day = String(dateObj.getDate()).padStart(2, "0");
+    const month = String(dateObj.getMonth() + 1).padStart(2, "0");
+    const year = dateObj.getFullYear();
+
+    const hours = String(dateObj.getHours()).padStart(2, "0");
+    const minutes = String(dateObj.getMinutes()).padStart(2, "0");
+    const seconds = String(dateObj.getSeconds()).padStart(2, "0");
+
+    return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+  }
+
+  return (
+    <>
+      <tr>
+        <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+          <div className="flex items-center">
+            <button onClick={() => {}}>
+              <a className="font-medium text-gray ">
+                {props.id}
+              </a>
+            </button>
+          </div>
+        </td>
+        <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+          <div className="text-center text-md font-medium text-gray">
+            {props.identifier}
+          </div>
+        </td>
+        <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+          <div className="text-center text-md font-medium text-gray">
+            {props.product}
+          </div>
+        </td>
+        <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+          <div className="text-center text-md font-medium text-gray">
+            {props.order}
+          </div>
+        </td>
+        <td className="px-2 first:pl-5 last:pr-5 py-6 whitespace-nowrap">
+          <div className="text-center text-md font-medium text-gray">
+            {props.quantity}
+          </div>
+        </td>
+        <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+          <div className="text-center text-md font-medium text-gray">
+            {formatDateTime(props.datetimeCreated)}
+          </div>
+        </td>
+        <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap">
+          <div className="text-center font-medium text-gray">
+            {props.sendToSAP === true && props.sapSuccess ? (
+              <div className="flex">
+                <Verify className="mr-2" color="#009B4A" size={20} />
+                <p className="text-primary">{props.sapStatus}</p>
+              </div>
+            ) : (
+              <div className="flex text-center">
+                <InfoCircle className="mr-2" color="gray" size={20} /> Información:
+                <p className="text-primary">{props.sapStatus}</p>
+              </div>
+            )}
+          </div>
+        </td>
+        <td className="px-2 first:pl-5 last:pr-5 py-3 whitespace-nowrap w-px">
+          {/* Menu button */}
+          {props.sapSuccess ? (
+            <button
+              onClick={(e) => {
+                e.stopPropagation();
+              }}
+              className="text-center font-semibold text-gray border btn border-slate-300 pointer-events-none w-full"
+            >
+              Reprocesar
+            </button>
+          ) : (
+            <button
+              aria-controls="danger-modal"
+              onClick={(e) => {
+                e.stopPropagation();
+                setDangerModalOpen(true);
+              }}
+              className="text-center font-semibold text-primary w-full"
+            >
+              Reprocesar
+            </button>
+          )}
+        </td>
+      </tr>
+      {/* Danger Modal */}
+      <div className="">
+        {/* Start */}
+
+        <ModalBlank
+          id="danger-modal"
+          modalOpen={dangerModalOpen}
+          setModalOpen={setDangerModalOpen}
+        >
+          <div className="p-5 flex space-x-4">
+            {/* Icon */}
+            <div className="w-10 h-10 rounded-full flex items-center justify-center shrink-0 bg-secondary">
+              <svg
+                className="w-4 h-4 shrink-0 fill-current text-primary"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 0C3.6 0 0 3.6 0 8s3.6 8 8 8 8-3.6 8-8-3.6-8-8-8zm0 12c-.6 0-1-.4-1-1s.4-1 1-1 1 .4 1 1-.4 1-1 1zm1-3H7V4h2v5z" />
+              </svg>
+            </div>
+            {/* Content */}
+            <div>
+              {/* Modal header */}
+              <div className="mb-2">
+                <div className="text-lg font-semibold text-slate-800">
+                  Reprocesar: {props.identifier}
+                </div>
+              </div>
+              {/* Modal content */}
+              <div className="text-sm mb-10">
+                <div className="space-y-2">
+                  <p className="text-black">
+                    ¿Estás seguro que deseas reprocesar el lote:{" "}
+                    {props.identifier}? Esta acción enviará el lote de nuevo al sistema SAP.
+                  </p>
+                </div>
+              </div>
+              {/* Modal footer */}
+              <div className="flex flex-wrap justify-end space-x-2">
+                <button
+                  className="btn-sm border-slate-200 hover:border-slate-300 text-slate-600"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setDangerModalOpen(false);
+                  }}
+                >
+                  Cancelar
+                </button>
+                <button
+                  className="btn-sm bg-primary hover:bg-primary-500 text-white"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    setDangerModalOpen(false);
+                    handleReprocess();
+                  }}
+                >
+                  Si, reprocesar
+                </button>
+              </div>
+            </div>
+          </div>
+        </ModalBlank>
+        {/* End */}
+      </div>
+    </>
+  );
+}
+
+export default LogsItem;
